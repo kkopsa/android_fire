3be01fd598852c905b1508623b4509133f505c3f
Kevin Kopsa <kopsak2@gmail.com>
2015-11-22 18:20:10 -0700

    mines table changes

--- app/src/main/java/com/kbt/dev/friendlyfire0/DatabaseHelper.java
+++ app/src/main/java/com/kbt/dev/friendlyfire0/DatabaseHelper.java
@@ -3,6 +3,7 @@ package com.kbt.dev.friendlyfire0;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by kevin on 7/1/15.
@@ -10,13 +11,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "friendly_fire";
-    private static final String MINE_TABLE_NAME = "mine_coordinates";
-    private static final String MINE_COLUMN_1 = "longitude";
-    private static final String MINE_COLUMN_2 = "latitude";
-    private static final String MINE_TABLE_CREATE =
-            "CREATE TABLE " + MINE_TABLE_NAME + " (" +
-                    MINE_COLUMN_1 + " REAL, " +
-                    MINE_COLUMN_2 + " REAL);";
+    private static final String MINES_TABLE_NAME = "mines";
+    private static final String MINES_COLUMN_PLAYER = "player_phone_number";
+    private static final String MINES_COLUMN_LATITUDE = "latitude";
+    private static final String MINES_COLUMN_LONGITUDE = "longitude";
+    private static final String MINES_COLUMN_PLANTED = "time_planted";
+    private static final String MINES_COLUMN_EXPLODED = "time_exploded";
+    private static final String MINES_TABLE_CREATE =
+            "CREATE TABLE IF NOT EXISTS" + MINES_TABLE_NAME + " (" +
+                    MINES_COLUMN_PLAYER + " TEXT," +
+                    MINES_COLUMN_LATITUDE + " REAL," +
+                    MINES_COLUMN_LONGITUDE + " REAL," +
+                    MINES_COLUMN_PLANTED + "TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+                    MINES_COLUMN_EXPLODED + "TIMESTAMP);";
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -28,11 +35,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(MINE_TABLE_CREATE);
+        db.execSQL(MINES_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(DatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + MINES_TABLE_NAME);
+        onCreate(db);
 
     }
 }
